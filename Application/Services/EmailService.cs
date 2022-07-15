8a0ddd933e99948fa1e8731d8e1eebad3eb91d8d
= <--add>
2022-07-15 03:03:54 +0300

    added email service

--- Application/Services/EmailService.cs
+++ Application/Services/EmailService.cs
@@ -1,17 +1,31 @@
 ﻿using Core.Interfaces.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MimeKit;
 
 namespace Application.Services
 {
     public class EmailService : IEmailService
     {
-        public Task Send(string email)
+        // ᓚᘏᗢ Use MailKit
+
+
+        const string address = "vetClinic224466@gmail.com";
+
+        public EmailService()
         {
-            throw new NotImplementedException();
+        }
+
+        public async Task Send(string email)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Vet clinic", address));
+            message.To.Add(new MailboxAddress("PotatoI9", email));
+
+            message.Subject = "Please, tell us what wrong";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = "You have left feedback that you aren't"
+            };
         }
     }
 }
